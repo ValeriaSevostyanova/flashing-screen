44142bfc437384e0265150f8da24388cd4d6ca67
Valeria Sevostyanova <125126489+ValeriaSevostyanova@users.noreply.github.com>
2023-12-26 22:19:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <windows.h>
+#include <string>
+#include <conio.h>
+
 
 int main() {
-    while (1) {    
-    SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
-    Sleep(2 * 1000);
-    SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1);
+    
+    bool flag = 0;
+    char key;
+
+    std::string password = "123"; // enter your password
+    std::string user_input = "";
+
+    while (1) {        
+        if (key == password[0] || flag) {
+            user_input += key;
+            flag = 1;
+        } if (password == user_input) {
+            break;
+        }
+        
+        SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+        Sleep(1000);
+        SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1);
+
+        key = getch();
     }
 
     return 0;
