155977ce25f0bbfbdefaf1e2c2eb9180b021f627
Valeria Sevostyanova <125126489+ValeriaSevostyanova@users.noreply.github.com>
2023-12-26 22:50:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <windows.h>
 #include <string>
 #include <conio.h>
-
+#include <ctime>
 
 int main() {
     
     bool flag = 0;
     char key;
+
+    unsigned int start_time = clock();
 
     std::string password = "123"; // enter your password
     std::string user_input = "";
@@ -15,7 +17,7 @@ int main() {
         if (key == password[0] || flag) {
             user_input += key;
             flag = 1;
-        } if (password == user_input) {
+        } if (password == user_input || start_time >= (1 * 60 * 1000)) {
             break;
         }
         
